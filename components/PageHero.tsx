afe978f8c15e71f8e1461ec4c80d63ab0ae43cbf
HLPFLCG <admin@hlpfl.co>
2025-11-17 19:29:13 -0500

    updatee

--- components/PageHero.tsx
+++ components/PageHero.tsx
@@ -22,7 +22,7 @@ export default function PageHero({ title, subtitle, imagePath }: PageHeroProps) 
           priority
         />
         {/* Overlay */}
-        <div className="absolute inset-0 bg-gradient-to-b from-black/60 via-black/40 to-black/60" />
+        <div className="absolute inset-0 bg-linear-to-b from-black/60 via-black/40 to-black/60" />
       </div>
 
       {/* Content */}
